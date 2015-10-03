81c0d08a80db1f25d8f7399f8aecada58df76b80
Hristo Dobtchev <chaosfire@gmail.com>
2015-10-03 21:25:15 +0300

    product sql done

--- shoppinglist.go
+++ shoppinglist.go
@@ -1,39 +1,73 @@
 package main
 
 import (
-	"database/sql"
+// 	"database/sql"
 	"log"
-	_ "github.com/lib/pq"
+	"fmt"
+	"strconv"
+	"net/http"
+	"github.com/krolim/shoppinglist/dbmanager"
+// 	_ "github.com/lib/pq"
 )
 
+type Hello struct{}
+
+func (h Hello) ServeHTTP(
+	w http.ResponseWriter,
+	r *http.Request) {
+	fmt.Fprint(w, "Hello!")
+}
+
+
 func main() {
-	var (
-		id int
-		type_id int
-		name string
-		description string
-		json string
-	)
-
-	db, err := sql.Open("postgres", "user=xxx password=xxx dbname=xxx sslmode=disable")
-	if err != nil {
+	// p := &dbmanager.Product{ Type_id: 1, 
+	// 						Title: "Bread 1", 
+	// 						Description: "Wholegrained bread 1", 
+	// 						Images: "{}"}
+	// err := dbmanager.AddProduct(p)
+	// err := dbmanager.RemoveProduct(1)
+	err,products := dbmanager.FetchAllProducts()
+	if (err != nil) {
 		log.Fatal(err)
+		panic("")
 	}
-	rows, err := db.Query("SELECT * FROM product")
-	if err != nil {
-			log.Fatal(err)
-		}
+	for i := 0; i < len(products); i++ {
+		fmt.Printf("row %v: %+v\n", i, products[i])	
+	}
 
-	defer rows.Close()
-	
-	for rows.Next() {
-		err := rows.Scan(&id, &type_id, &description, &name, &json)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Println(id, name, description, type_id, json)
-	}
-	err = rows.Err()
+	http.HandleFunc("/products", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		switch r.Method {
+			case "DELETE":
+				if len(r.Form["id"]) == 0  {
+					w.WriteHeader(http.StatusBadRequest)
+					fmt.Fprintf(w, "Missing id")
+
+				} else {
+					id,err := strconv.ParseInt(r.Form["id"][0], 0, 64)
+					if (err != nil) {
+						w.WriteHeader(http.StatusBadRequest)
+						fmt.Fprintf(w, "Invalid id %v", err)
+					} else {
+						dbmanager.RemoveProduct(int(id))
+						fmt.Fprintf(w, "Deleted: %v", r.Form["id"][0])
+					}
+				}
+			default:
+				err,products := dbmanager.FetchAllProducts()
+				if (err != nil) {
+					log.Fatal(err)
+					panic("")
+				}
+				for i := 0; i < len(products); i++ {
+					fmt.Fprintf(w, "row %v: %+v\n", i, products[i])	
+				}
+							// fmt.Fprintf(w, "Welcome to the bar %v", r.URL.Path)	
+		}		
+					
+	})
+	// var h Hello
+	err = http.ListenAndServe("localhost:4000", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
